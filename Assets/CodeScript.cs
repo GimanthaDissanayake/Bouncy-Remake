87fa84936a2bca3466f74c8ede87224e0d763e9d
Gimantha Dissanayake <gimanthad@gmail.com>
2025-11-16 15:04:14 +0200

    Unity Project Creation

---
+++ Assets/CodeScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeScript : MonoBehaviour
+{
+    void Start()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
